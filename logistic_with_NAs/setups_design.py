4231e993f80d84d54ad217c3cfcaf063faa0b584
mullerchristophe21 <mullerchristophe21@gmail.com>
2025-07-17 16:22:24 +0200

    update config for plots #16

--- logistic_with_NAs/setups_design.py
+++ logistic_with_NAs/setups_design.py
@@ -21,9 +21,10 @@ color_palette = sns.color_palette()
 
 methods_config = {
 
-    "SAEM": {"label": "SAEM", "color": color_palette[5], "linestyle": "-", "marker":"D"}, # Changed marker
+    "SAEM": {"label": "R.SAEM", "color": color_palette[5], "linestyle": "-", "marker":"D"}, # Changed marker
     "py.SAEM": {"label": "py.SAEM", "color": color_palette[1], "linestyle": "--", "marker":"D"},
     "py.SAEM.fast": {"label": "py.SAEM.fast", "color": color_palette[2], "linestyle": "-.", "marker":"D"},
+    "py.SAEM.fast.fixed": {"label": "py.SAEM.fast.fixed", "color": color_palette[0], "linestyle": "-", "marker":"D"},
     "py.SAEM.parallel": {"label": "py.SAEM.parallel", "color": color_palette[3], "linestyle": ":", "marker":"D"},
     "py.SAEM.parallel.fast": {"label": "py.SAEM.parallel.fast", "color": color_palette[4], "linestyle": "-", "marker":"D"},
 }
